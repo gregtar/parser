0a8eb4bac502ccc2d22b5a79281a848bd9251bad
gregtar <gregarty@gmail.com>
2017-09-19 11:14:35 +0300

    Добавлен телефон и прайслист

--- main.py
+++ main.py
@@ -21,12 +21,19 @@ for page in range(1,2): #page + 1
     for item in data:
         data = soup.find_all('ul', {'class': 'result_'})
         companyName = item.find('a', {'class': 'name_des i'}).text
-        information = item.find('div', {'class': 'snipet'}).text
+        activities= item.find('li', {'class': 'span3'}).text
+      #  information = item.find('div', {'class': 'snipet'}).text
         city = item.find('div', {'class': 'result_right'}).text
-      #  site = item.find('ul', {'class': 'l-item-features'}).contents[-2].text
-      #  phone = item.find('ul', {'class': 'result_'})
-      
-#
+        phone = soup.select("li > div:nth-of-type(2)")
+        price = soup.select("li > div > div:nth-of-type(1)")
+
+
+           #div:nth-child(3)
+
+
+
+
+ #div:nth-child(3)
 
      #  priceList = item.find('a',{'class':'i'}).text
 
@@ -34,28 +41,27 @@ for page in range(1,2): #page + 1
 
         companies.append({
         'companyName':companyName,
-        'information':information,
+        'activities': activities,
         'city': city,
-        # 'site':site,
-        # 'phone':phone,
-       #  'priceList':priceList
+        #'information': information,
+        'phone':phone,
+        'price':price
         })
 
-        # center_u > div > div > div > div > div > div.span9 > div.span7 > ol > li:nth-child(1) > div:nth-child(3)
-        # center_u > div > div > div > div > div > div.span9 > div.span7 > ol > li:nth-child(3) > div:nth-child(3)
 
 
 for company in companies:
      print('Название компании:', company['companyName'])
-     print('Информация про компанию:', company['information'])
-     print('Город:',company['city'])
-   #  print('Телефон',company['phone'])
+     print('Деятельность', company['activities'])
+     print('Город:', company['city'])
+     #print('Информация про компанию:', company['information'])
+     print('Телефон:',company['phone'])
+   #  print('Прайс Лист',company['price'])
+     print('-------------------------------------------------------------------')
 
 
-  #  print('Сайт:',company['site'])
-   # print('Телефон:', company['phone'])
-   # print('Ссылка на прайслит:', company['priceList'])
-   # print()
+  # print('Сайт:',company['site'])
+  # print()
 
 with open(FILENAME, 'w',) as csv_file:
     csv_writer = csv.writer(csv_file)
@@ -64,4 +70,4 @@ with open(FILENAME, 'w',) as csv_file:
 
 
 #print(companies)
-#print(phone)
+#print(activities)
